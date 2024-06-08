77c21f7a379a1d7d44c36e50bde688ee85f31b4e
ArpitTripathi945 <ad3617@srmist.edu.in>
2024-06-08 11:48:42 +0530

    REST_API

---
+++ src/main/java/com/restapp/journal/controller/JournalEntryController.java
@@ -0,0 +1,45 @@
+package com.restapp.journal.controller;
+
+import com.restapp.journal.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("journal")
+public class JournalEntryController {
+
+
+    private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long myId, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry);
+    }
+
+
+}
